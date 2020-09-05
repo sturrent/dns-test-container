09ab074ee7f0f7b33cbdef1034386eb0ea993c89
Sergio Turrent <seturren@microsoft.com>
2020-09-05 15:41:35 -0600

    Updated runner script version

--- dns-plot/runner.sh
+++ dns-plot/runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # script name: resperf-script.sh
-# Version v0.1.2 20200905
+# Version v0.1.3 20200905
 # DNS test to check resolution time
 # Uses two arguments, first one to specify max queries per second and second one to specify the DNS server IP to query
 
@@ -26,7 +26,7 @@ else
 fi
 
 pushd /dns-plot > /dev/null
-cp index.html_base index.html > /dev/null
+cp index.html_base index.html 2> /dev/null
 rm -f $(ls | grep '202\|dnsperf-out') &>/dev/null
 resperf-report -s $DNS_SERVER -d queryfile-sample1 -i 0.5 -m $MAX_QPS -L 1
 mv $(ls | grep html | head -1) index.html
