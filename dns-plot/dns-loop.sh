689c57bb871f0f719b79c425fd546b2a583add5b
Sergio Turrent <seturren@microsoft.com>
2020-09-18 07:35:22 -0600

    Updated dns-loop script

--- dns-plot/dns-loop.sh
+++ dns-plot/dns-loop.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-loops=$(($1+0))
-for ((i=1;i<=$loops;i++))
-do
-        dt=`date '+%d/%m/%Y %H:%M:%S'`
-        #echo "$dt"
-        #curl -A `date +"[mycurltime:%Y-%m-%dT%H:%M:%S.%3N]"` -X GET 'https://www.google.com' --silent --output /dev/null -w " status=%{http_code} %{redirect_url} size=%{size_download} time_namelookup=%{time_namelookup} time_connect=%{time_connect} time_appconnect=%{time_appconnect} time_pretransfer=%{time_pretransfer} time_redirect=%{time_redirect}  time_starttransfer=%{time_starttransfer} time=%{time_total} num_redirects=%{num_redirects} speed_download=%{speed_download} speed_upload=%{speed_upload} num_connects=%{num_connects}  content-type="%{content_type}" "
-        echo "$(curl -X GET 'https://www.google.com' --silent --output /dev/null -w "%{time_namelookup}")"
-        sleep 0.3
+NumberOfTests=$1
+URL=$2
+SleepDuration=$3
+
+#Check Vars
+if [[ -z $1 ]] ; then NumberOfTests="10" ; fi
+if [[ -z $2 ]] ; then URL="www.google.com" ; fi
+if [[ -z $3 ]] ; then SleepDuration="0.250" ; fi
+
+
+echo -e "\e[34mTesting URL \"$URL\" $NumberOfTests times every $SleepDuration s\033[m"
+
+#Init Var
+count=0
+TotalResult=0
+AverageResult=0
+MinResult=10000
+MaxResult=0
+
+while [ $count -lt $NumberOfTests ] ; do
+
+  #time_namelookup is the time the conversion 'name to ip' takes. many different options are possible for curl
+  ReturnvalueIsS=$(curl --output /dev/null --silent --write-out '%{time_namelookup}' $URL)
+  #Convert S to MS
+  Returnvalue=$(awk "BEGIN {print $ReturnvalueIsS*1000}")
+
+  echo -ne "Current response time : $Returnvalue ms              "\\r
+
+  TotalResult=$(awk "BEGIN {print ($TotalResult + $Returnvalue)}")
+  MinResult=$(echo $Returnvalue $MinResult | awk '{if ($1 < $2) print $1; else print $2}')
+  MaxResult=$(echo $Returnvalue $MaxResult | awk '{if ($1 > $2) print $1; else print $2}')
+
+  sleep $SleepDuration
+  ((count=count+1))
 done
+
+AverageResult=$(awk "BEGIN {print $TotalResult / $count}")
+echo "AverageResult = $AverageResult ms | Min value = $MinResult ms | Max value = $MaxResult ms"
+
+exit 0
